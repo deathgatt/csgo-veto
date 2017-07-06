f6adc4e4b64fe627048b03cb904ad0a435bedcdc
Matthieu <contact@hizen.fr>
2017-07-06 02:19:32 +0200

    Add Firebase config

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBqeHPUtD_aChNKmg4oldt_K7IBqQhKLZw",
+  authDomain: "csgo-veto-5a0cb.firebaseapp.com",
+  databaseURL: "https://csgo-veto-5a0cb.firebaseio.com",
+  projectId: "csgo-veto-5a0cb",
+  storageBucket: "csgo-veto-5a0cb.appspot.com",
+  messagingSenderId: "656665795388"
+};
+firebase.initializeApp(config);
